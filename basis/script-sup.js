fd1c2681e524206ae42246b41ac7837af87fc421
elsoghiar <mahmoudhouseen555@gmail.com>
2025-02-28 12:22:56 +0200

    Update script-sup.js

--- basis/script-sup.js
+++ basis/script-sup.js
@@ -1,11 +1,3 @@
-function showLoadingIndicator() {
-   document.getElementById("loadingOverlay").style.display = "block";
-}
-
- function hideLoadingIndicator() {
-   document.getElementById("loadingOverlay").style.display = "none";
-}
-
 document.addEventListener("DOMContentLoaded", initializeEventListen);
 
 function initializeEventListen() {
