67a133c274598e8162f16eeef664cd56345038f2
elsoghiar <mahmoudhouseen555@gmail.com>
2025-03-03 00:39:38 +0200

    Update script.js

--- basis/script.js
+++ basis/script.js
@@ -373,9 +373,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 let map = L.map('map').setView([20, 0], 2);
-L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://stamen.com/">Stamen Design</a>',
-    subdomains: 'abcd',
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '&copy; <a href="https://www.esri.com/">Esri</a>',
     maxZoom: 20
 }).addTo(map);
 
